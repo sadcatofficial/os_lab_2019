51ddaa0ed2706a3950aafd92961c73b89b018e77
sadcatofficial <lumodder1@yandex.ru>
2020-04-12 21:02:50 +0000

    добаÐlab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
 #include "revert_string.h"
-
+#include <stdio.h>
 void RevertString(char *str)
 {
-	// your code here
+int i;
+char reverse[strlen(str)];
+for (i=0;i<strlen(str);i++)
+reverse[i]=str[strlen(str)-i-1];
+reverse[strlen(str)] = '\0';
+for (i=0;i<strlen(str);i++)
+str[i] = reverse[i] ;
+
 }
 
